dce82cde1a328e57ac98916976086c2cee4c8d71
kallgueye <gueyekallamoussa@gmail.com>
2025-06-25 05:15:46 +0000

    week 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
-use std::{f32::consts::E, fmt::format, io::Read};
-
 use hex::{decode, encode};
 
 pub fn decode_hex(hex_str: &str) -> Result<Vec<u8>, String> {
     // TODO: Decode hex string into Vec<u8>, return error string on failure
     match decode(hex_str) {
         Ok(e) => Ok(e),
-        Err(e) => Err(e.to_string())
+        Err(e) => Err(e.to_string()),
     }
 }
 
@@ -14,7 +12,7 @@ pub fn to_big_endian(bytes: &[u8]) -> Vec<u8> {
     // TODO: Reverse the byte order of input slice and return as Vec<u8>
     let mut v = bytes.to_vec();
     v.reverse();
-    return v; 
+    v
 }
 
 pub fn bytes_to_hex(bytes: &[u8]) -> String {
@@ -47,13 +45,12 @@ pub enum ScriptType {
 }
 
 pub fn classify_script(script: &[u8]) -> ScriptType {
-   match script {
+    match script {
         [0x76, 0xA9, 0x14] => ScriptType::P2PKH,
         [0x00, 0x14, 0xff] => ScriptType::P2WPKH,
-      _ => ScriptType::Unknown,
-   }
+        _ => ScriptType::Unknown,
+    }
 }
-
 
 // TODO: complete Outpoint tuple struct
 pub struct Outpoint(pub String, pub u32);
@@ -86,7 +83,6 @@ pub fn apply_fee(balance: &mut u64, fee: u64) {
 pub fn move_txid(txid: String) -> String {
     // TODO: Return formatted string including the txid for display or logging
     format!("txid: {}", txid)
-
 }
 
 // TODO: Add necessary derive traits
@@ -102,7 +98,7 @@ impl Opcode {
         // TODO: Implement mapping from byte to Opcode variant
         match byte {
             0xAC => Ok(Opcode::OpChecksig), // Hex for OP_CHECKSIG
-            0x76 => Ok(Opcode::OpDup),      // Hex for OP_DUP   
+            0x76 => Ok(Opcode::OpDup),      // Hex for OP_DUP
             _ => Err("Invalid opcode: 0x00".to_string()),
         }
     }
